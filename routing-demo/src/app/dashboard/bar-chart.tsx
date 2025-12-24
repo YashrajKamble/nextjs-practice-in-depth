50e307eb99159ec5c80cc55074b1219ed5151d66
Yashraj_K <yashrajkamble7727@gmail.com>
2025-12-24 16:18:34 +0530

    File Colocation

---
+++ routing-demo/src/app/dashboard/bar-chart.tsx
@@ -0,0 +1,3 @@
+export default function BarChart() {
+    return <h1>bar chart</h1>
+}
---
+++ routing-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+function DashBoard1() {
+    return <h1>dashboard</h1>
+}
+
+export default function DashBoard() {
+    return <h1>dashboard</h1>
+}
