65b5344658e9a44c8ebd0817934431c7f3cf329d
Yashraj_K <yashrajkamble7727@gmail.com>
2025-12-24 17:02:36 +0530

    Layouts

--- routing-demo/src/app/layout.tsx
+++ routing-demo/src/app/layout.tsx
@@ -10,7 +10,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "1rem"
+          }}
+        >HEADER
+        </header>
+
+        {children}
+
+        <footer
+          style={{
+            backgroundColor: "ghostwhite",
+            padding: "1rem"
+          }}
+        >FOOTER
+        </footer>
+      </body>
     </html>
   )
 }
