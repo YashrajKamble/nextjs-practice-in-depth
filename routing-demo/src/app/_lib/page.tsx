87b373724ddf4ca752921998805ded91e1959c5c
Yashraj_K <yashrajkamble7727@gmail.com>
2025-12-24 16:28:59 +0530

    Private Folders

---
+++ routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,6 @@
+export default function PrivateRoute() {
+    return <>
+        <h1> You cant view this in the browser</h1>
+        <h1>to see "_lib" path, rename it to "%5Flib" in URL then it shows actual values in the "_lib" file</h1>
+    </>
+}
