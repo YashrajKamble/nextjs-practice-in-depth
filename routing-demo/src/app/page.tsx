5660d98a8b396bfbc87f04d13bf15aa7c26a4140
Yashraj_K <yashrajkamble7727@gmail.com>
2025-12-25 17:22:39 +0530

    Linking Component Navigation

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import Link from "next/link";
 
 const Home = () => {
     return (
-        <div>Welcome Home! </div>
+        <>
+            <div>Welcome Home! </div>
+            <Link href={"/blog"}>Blog</Link>
+            <br />
+            <Link href={"/products"}>Products</Link>
+        </>
     )
 }
 
--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,12 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Products = () => {
+    const productId = 100;
     return (
         <div>
-            <h1>Product List</h1>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <Link href={"/"}>Home</Link>
+            <h1>LinkProduct List</h1>
+            <h2>
+                <Link href="products/1">Product 1</Link>
+            </h2>
+            <h2>
+                <Link href="products/2">Product 2</Link>
+            </h2>
+            <h2>
+                <Link href="products/3" replace>Product 3</Link>
+            </h2>
+            <h2>
+                <Link href={`/products/${productId}`}>{productId}</Link>
+            </h2>
         </div>
     )
 }
