202a0fb1e5ce84b616f5bff009d456258212c6bf
Yashraj_K <yashrajkamble7727@gmail.com>
2025-12-26 17:27:40 +0530

    Templates

--- routing-demo/src/app/(auth)/layout.tsx
+++ routing-demo/src/app/(auth)/layout.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import "./styles.css";
+import { useState } from "react";
 
 const navLink = [
     { name: "Register", href: "/register" },
@@ -15,9 +16,13 @@ export default function AuthLayout({
 }: {
     children: React.ReactNode;
 }) {
+    const [input, setInput] = useState("");
     const pathName = usePathname();
     return (
         <div>
+            <div>
+                <input value={input} onChange={e => setInput(e.target.value)} />
+            </div>
             {navLink.map((link) => {
                 const isActive = pathName === link.href || (pathName.startsWith(link.href) && link.href !== "/");
                 return (
@@ -31,3 +36,7 @@ export default function AuthLayout({
         </div>
     )
 }
+
+// IF WE CHANGES NAME OF THIS FILE FROM LAYOUT.TSX TO TEMPLATE.TSX , THEN THAT TIME WE NOT ABLE TO SEE THE SEE VALUE IN THE INPUT FILES WHEN WE MOVE FROM LOGIN OR FORGOT OR REGISTER PAGE LIKE HOW WE SEE IT IN LAYOUT.TSX
+
+// SO  SEE VALUES IN LAYOUT.TSX FILE NAME AND NOT FOR TEMPLATE.TSX
