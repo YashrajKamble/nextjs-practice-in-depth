0e22aff837a37db47d1b9600ae7276c6bdee0ca2
Yashraj_K <yashrajkamble7727@gmail.com>
2026-01-01 15:25:36 +0530

    Headers in Route Handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-  return new Response("Profile API Fetching");
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // const requestHeader = new Headers(request.headers);
+  // console.log(requestHeader.get("Authorization"));
+
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>Profile API Fetching<h1/>",{
+    headers:{
+      "Content-type":"text/html"
+    }
+  });
 }
