ee05cd7a672dfb4f0520fe2909d5c652d195c714
Yashraj_K <yashrajkamble7727@gmail.com>
2026-01-07 16:19:16 +0530

    Dynamic Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function aboutPage() {
+import { cookies } from "next/headers"
+
+export default async function aboutPage() {
+    const cookiesStore = await cookies()
+    const theme = cookiesStore.get("theme")
+    console.log(theme);
     console.log("about server component")
     return <h1>About Page{new Date().toLocaleTimeString()}</h1>
 }
