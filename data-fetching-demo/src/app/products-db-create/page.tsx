66ac43d48f742678e5155ea050892d5b49485e7c
Yashraj_K <yashrajkamble7727@gmail.com>
2026-01-21 17:06:07 +0530

    useActionState Hook

--- data-fetching-demo/src/app/products-db-create/page.tsx
+++ data-fetching-demo/src/app/products-db-create/page.tsx
@@ -1,8 +1,25 @@
 import { Submit } from "@/components/submit";
 import { addProduct } from "@/prisma-db";
 import { redirect } from "next/navigation";
+import { useActionState } from "react";
+
+type Errors = {
+    title?: string
+    price?: string
+    description?: string
+}
+
+type FormState = {
+    errors: Errors
+}
 
 export default function AddProductPage() {
+    const initialState: FormState = {
+        errors: {}
+    }
+
+    const [state, formAction, isPending] = useActionState(createProduct, initialState)
+
     async function createProduct(formData: FormData) {
         "use server"
 
@@ -10,37 +27,70 @@ export default function AddProductPage() {
         const price = formData.get("price") as string;
         const description = formData.get("description") as string;
 
+        const errors: Errors = {};
+
+        if (!title) {
+            errors.title = "title is required"
+        }
+        if (!price) {
+            errors.title = "price is required"
+        }
+        if (!description) {
+            errors.title = "description is required"
+        }
+
+        if (Object.keys(errors).length > 0) {
+            return { errors }
+        }
+
         await addProduct(title, parseInt(price), description)
         redirect("/products-db")
     }
 
 
     return (
-        <form action={createProduct} className="p-4 space-y-4 max-w-96">
-            <label className="text-white">
-                Title
-                <input
-                    type="text"
-                    className="block w-full p-2 text-black border rounded"
-                    name="title"
-                />
-            </label>
-            <label className="text-white">
-                Price
-                <input
-                    type="number"
-                    className="block w-full p-2 text-black border rounded"
-                    name="price"
-                />
-            </label>
-            <label className="text-white">
-                Description
-                <textarea
-                    className="block w-full p-2 text-black border rounded"
-                    name="description"
-                />
-            </label>
-            <Submit />
+        <form action={formAction} className="p-4 space-y-4 max-w-96">
+            <div>
+                <label className="text-white">
+                    Title
+                    <input
+                        type="text"
+                        className="block w-full p-2 text-black border rounded"
+                        name="title"
+                    />
+                </label>
+                {state.errors.title && <p className="text-red-500">{state.errors.title}</p>}
+            </div>
+
+            <div>
+                <label className="text-white">
+                    Price
+                    <input
+                        type="number"
+                        className="block w-full p-2 text-black border rounded"
+                        name="price"
+                    />
+                </label>
+                {state.errors.price && <p className="text-red-500">{state.errors.price}</p>}
+            </div>
+
+            <div>
+                <label className="text-white">
+                    Description
+                    <textarea
+                        className="block w-full p-2 text-black border rounded"
+                        name="description"
+                    />
+                </label>
+                {state.errors.description && <p className="text-red-500">{state.errors.description}</p>}
+            </div>
+            {/* <Submit /> */}
+            <button
+                type="submit" className="block w-full p-2 text-white bg-blue-500 rounded disabled:bg-gray-500"
+                disabled={isPending}>
+                Submit
+            </button>
         </form>
+
     );
 }
