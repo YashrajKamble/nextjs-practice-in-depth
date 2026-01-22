0e4ce55b8aef479e8a828c457bf15e3364ee09b0
Yashraj_K <yashrajkamble7727@gmail.com>
2026-01-22 14:57:40 +0530

    Optimistic Update with useOptimistic Hook

--- data-fetching-demo/src/app/products-db/page.tsx
+++ data-fetching-demo/src/app/products-db/page.tsx
@@ -1,7 +1,5 @@
-
-import { removeProduct } from "@/actions/products";
 import { getProducts } from "../../prisma-db";
-import Link from "next/link";
+import { ProductDetails } from "./products-details";
 
 export type Product = {
     id: number;
@@ -10,32 +8,10 @@ export type Product = {
     description: string | null;
 };
 
-export const dynamic = 'force-dynamic';
-
 export default async function ProductsDBPage() {
     const products: Product[] = await getProducts();
 
     return (
-        <ul className="space-y-4 p-4">
-            {products.map((product) => (
-                <li
-                    key={product.id}
-                    className="p-4 bg-white shadow-md rounded-lg text-gray-700"
-                >
-                    <h2 className="text-xl font-semibold">
-                        <Link href={`/products-db/${product.id}`}>{product.title}</Link>
-                    </h2>
-                    <p>{product.description}</p>
-                    <p className="text-lg font-medium">${product.price}</p>
-                    <form action={removeProduct.bind(null, product.id)}>
-                        <button
-                            type="submit" className="px-4 py-2 mt-4 text-white bg-red-500 rounded-md hover:bg-red-600"
-                        >
-                            Delete
-                        </button>
-                    </form>
-                </li>
-            ))}
-        </ul>
+        <ProductDetails products={products} />
     );
 }
