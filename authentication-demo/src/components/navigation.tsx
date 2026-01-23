43991a7fd30f09ee75de75f91c781c2634b14975
Yashraj_K <yashrajkamble7727@gmail.com>
2026-01-23 16:11:20 +0530

    Conditional UI Rendering

--- authentication-demo/src/components/navigation.tsx
+++ authentication-demo/src/components/navigation.tsx
@@ -1,4 +1,4 @@
-import { SignInButton, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export const Navigation = () => {
@@ -12,11 +12,16 @@ export const Navigation = () => {
                         </h1>
                     </div>
                     <div className="flex items-center gap-4">
-                        <SignInButton mode="modal" />
-                        <Link href="/user-profile">
-                            Profile
-                        </Link>
-                        <SignOutButton />
+                        <SignedOut>
+                            <SignInButton mode="modal" />
+                        </SignedOut>
+
+                        <SignedIn>
+                            <Link href="/user-profile">
+                                Profile
+                            </Link>
+                            <SignOutButton />
+                        </SignedIn>
                     </div>
                 </div>
             </div>
